48153fc13303d656fb1cd69a788c8a51a19fc338
gejunwen <gejunwen@gejunwendeMacBook-Pro.local>
2016-03-26 02:35:19 +0800

    entering chapter02

---
+++ ch02/list.py
@@ -0,0 +1,12 @@
+#! /usr/local/bin/python
+
+edward = ['Edward Gumby', '42']
+john = ['John Smith', 50]
+
+database = [edward, john]
+
+print(database)
+
+print(edward[0], edward[-1])
+
+print("Hello"[1])
